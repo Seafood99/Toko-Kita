1720f0fea1af29b75be05ae6e919028c36f1684b
Dawam <muhammadawamali@gmail.com>
2024-08-15 20:20:55 +0700

    membuat halaman about

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function About() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center mt-10">About Us</h1>
+            <p className="text-center mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae?</p>
+        </div>
+    )
+}
